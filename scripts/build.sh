1e1dd888984a30c4ebe2f97ceb7a5863b878e4f6
curtself <curtself.cs@gmail.com>
2026-09-07 18:35:17 -0700

    Updates to the build script and release action to build Windows version.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,19 @@ VERSION="$(cat VERSION)"
 COMMIT="$(git rev-parse --short HEAD)"
 BUILD_DATE="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
 
+OS="${1:-$(go env GOOS)}"
+ARCH="${2:-$(go env GOARCH)}"
+
 mkdir -p dist
 
 go fmt ./...
 go vet ./...
 #go test ./...
 
-#CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X 'main.Version=${VERSION}' -X 'main.Commit=${COMMIT}' -X 'main.BuildDate=${BUILD_DATE}'" -o "dist/tlsg"
-CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X 'tlsg/internal/version.Version=${VERSION}' -X 'tlsg/internal/version.Commit=${COMMIT}' -X 'tlsg/internal/version.BuildDate=${BUILD_DATE}'" -o "dist/tlsg"
+OUTPUT="dist/tlsg-${OS}-${ARCH}"
+if [[ "${OS}" == "windows" ]]; then 
+  OUTPUT+=".exe" 
+fi
+
+CGO_ENABLED=0 GOOS="${OS}" GOARCH="${ARCH}" go build -trimpath -ldflags="-s -w -X 'tlsg/internal/version.Version=${VERSION}' -X 'tlsg/internal/version.Commit=${COMMIT}' -X 'tlsg/internal/version.BuildDate=${BUILD_DATE}'" -o "${OUTPUT}"
+
